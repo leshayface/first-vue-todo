42a74f157adc2b85c02db9608835b1b7c1974a22
leface <leshaywebdzn@gmail.com>
2020-10-28 17:12:03 +0200

    feat(App): implement vuex

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -2,23 +2,23 @@
   <div>
     <div class="flex flex-row">
       <div class="w-1/5 flex items-center">
-        <input type="checkbox" v-model="completed" @change="doneEditing" /> <!-- bind completed property to checkbox -->
+        <input type="checkbox" v-model="completed" @change="doneEdit" /> <!-- bind completed property to checkbox -->
       </div>
-      <div class="w-3/5">
-        <p v-bind:class="{'line-through': todo.completed}" v-if="!editing" @dblclick="editTodo(todo)">{{title}}</p> <!-- if editing isnt true, display the todo item -->
+      <div class="w-3/5" @click="completeTodo">
+        <p v-bind:class="{'line-through': completed}" v-if="!editing" @dblclick="editTodo(todo)">{{title}}</p> <!-- if editing isnt true, display the todo item -->
         <!-- if editing is true, display the editing input. End editing on enter / blur. On escape key call cancelEditing method -->
         <input
           class="appearance-none bg-transparent border-none w-full text-gray-700 py-1 px-2 leading-tight focus:outline-none text-center"
           v-else type="text"
           v-model="title"
-          @blur="doneEditing(todo)"
-          @keyup.enter="doneEditing(todo)"
+          @blur="doneEdit(todo)"
+          @keyup.enter="doneEdit(todo)"
           @keyup.esc="cancelEditing(todo)"
           v-focus
         />
       </div>
       <div class="w-1/5 text-right">
-        <h3 @click="removeTodo(index)">X</h3> <!-- on click call removeTodo method -->
+        <h3 @click="removeTodo(todo.id)">X</h3> <!-- on click call removeTodo method -->
       </div>
     </div>
   </div>
@@ -30,10 +30,6 @@
     props: {
       todo: {
         type: Object,
-        required: true,
-      },
-      index: {
-        type: Number,
         required: true,
       }
     },
@@ -47,6 +43,7 @@
       }
     },
     directives: {
+      // makes cursor focus on dblclick
       focus: {
         inserted: function (el) {
           el.focus()
@@ -54,23 +51,31 @@
       }
     },
     methods: {
-      removeTodo(index) {
-        this.$emit('removedTodo', index)
+      removeTodo(id) {
+        this.$store.dispatch('deleteTodo', id)
       },
       editTodo() {
         this.beforeEditCache = this.title;
         this.editing = true;
       },
-      doneEditing() {
+      doneEdit() {
         //if edited title is empty set it to the previous text
         if (this.title.trim() == '') {
           this.title = this.beforeEditCache
         }
         this.editing = false
-        this.$emit('finishedEdit', {
+        this.$store.dispatch('updateTodo', {
           'id': this.id,
           'title': this.title,
           'completed': this.completed,
+          'editing': this.editing,
+        })
+      },
+      completeTodo() {
+        this.$store.dispatch('updateTodo', {
+          'id': this.id,
+          'title': this.title,
+          'completed': this.completed = true,
           'editing': this.editing,
         })
       },
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-1/2">
-    <div class="w-auto flex items-center border-b border-teal-500 py-2 m-6">
+  <div class="md:w-1/2">
+    <div class="w-auto flex items-center border-b border-teal-500 py-2 my-6">
       <input
         class="appearance-none bg-transparent border-none w-full text-gray-700 py-1 px-2 leading-tight focus:outline-none"
         type="text"
@@ -10,84 +10,59 @@
       /> <!-- use v-model to bind data property -->
     </div>
     <p>TODO LIST</p>
-    <todo
-      class="border border-gray-400 rounded-b flex flex-col justify-between leading-normal m-6 p-4"
-      v-for="(todo, index) in todos"
-      :key="todo.id" :todo="todo"
-      :index="index"
-      @removedTodo="removeTodo"
-      @finishedEdit="finishedEdit"
-    > <!-- grab each item from array as well as its index -->
-    </todo>
-    <div>{{ remaining }} items left</div>
+    <div class="space-y-4 my-6">
+      <todo
+        class="border border-gray-400 rounded-b flex flex-col justify-between leading-normal p-4"
+        v-for="todo in todos"
+        :key="todo.id"
+        :todo="todo"
+      > <!-- grab each item from array as well as its index -->
+      </todo>
+    </div>
+    <todo-items-remaining></todo-items-remaining>
      <!-- <div>{{ remaining }} items left</div> computed properties can be referred to like data properties -->
   </div>
 </template>
 
 <script>
 import Todo from './Todo'
+import TodoItemsRemaining from './TodoItemsRemaining'
 
 export default {
   name: 'todo-list',
   components: {
     Todo,
+    TodoItemsRemaining,
   },
   data () {
     return {
       newTodo: '',
       beforeEditCache: '',
       idForTodo: 3,
-      todos: [
-        {
-          'id': 1,
-          'title': 'Finish Building This App',
-          'completed': false,
-          'editing': false
-        },
-        {
-          'id': 2,
-          'title': 'Take Over The World!',
-          'completed': false,
-          'editing': false
-        },
-      ]
     }
   },
   methods: {
     addTodo() {
       //use this keyword to access data
-
       //make sure no empty todo can be added
       if (this.newTodo.trim().length == 0) {
         return
       }
-
-      console.log(this.idForTodo);
-
-      this.todos.push(
-        {
-          id: this.idForTodo, //sets the new todo id to 3
-          title: this.newTodo, //grab text from input (v-model) - two way data binding
-          completed: false,
-          editing: false
-        }
-      )
-
+      this.$store.dispatch('addTodo', {
+        id: this.idForTodo, //sets the new todo id to 3
+        title: this.newTodo, //grab text from input (v-model) - two way data binding
+      })
       this.newTodo = ''; //set newTodo text to empty
       this.idForTodo++; //increment idForTodo for next todo
-    },
-    removeTodo(index) {
-      this.todos.splice(index, 1); //splice todo array by index and remove 1 item
-    },
-    finishedEdit(data) {
-      const index = this.todos.findIndex((item) => item.id == data.id)
-      this.todos.splice(index, 1, data)
     }
   },
   computed: {
-    remaining() {
-      return this.todos.filter(todo => !todo.completed).length
+    anyRemaining() {
+      return this.$store.getters.anyRemaining
     },
+    todos() {
+      return this.$store.getters.todos
+    }
   }, //computed property is for composing new data derived from other data
 }
 </script>
